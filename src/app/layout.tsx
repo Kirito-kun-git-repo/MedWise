5493e3e3efc60c22f909f70e0c5e6fdca305abea
Kirito <kiritokunonline21@gmail.com>
2025-10-15 19:25:11 +0530

    users-synced-with-db

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import UserSync from "@/components/ui/UserSync";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
+        <UserSync/>
         {children}
+        
+
       </body>
     </html>
     </ClerkProvider>
